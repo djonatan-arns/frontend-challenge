1c381c4791971b3a26fb6a92f23707f65128a0e7
Djonatan Arns <djonatan.code@gmail.com>
2026-02-07 10:29:40 -0300

    feat: enhance FilePreview with image support using pictureSvg asset

--- src/components/FilePreview/index.tsx
+++ src/components/FilePreview/index.tsx
@@ -1,3 +1,4 @@
+import pictureSvg from '@/assets/picture.svg'
 import './styles.css'
 
 interface FilePreviewProps {
@@ -8,7 +9,9 @@ interface FilePreviewProps {
 export function FilePreview({ fileName, fileSize }: FilePreviewProps) {
   return (
     <div className="file-preview">
-      <div className="file-preview__image" />
+      <div className="file-preview__image">
+        <img src={pictureSvg} alt={fileName} className="file-preview__img" />
+      </div>
       <div className="file-preview__info">
         <span className="file-preview__name">{fileName}</span>
         <span className="file-preview__size">{fileSize}</span>
